4f872c1034a722f920563b9d5bf37936c5537ab4
Santiago <santiago.gonzalezagudelo@gmail.com>
2023-04-27 09:01:51 -0500

    nuevos cambios

---
+++ clase1.py
@@ -0,0 +1,34 @@
+nombre  = "juanes"
+edad = 15
+solter = True
+salario = 1.800000
+
+colegio  = "pilar"
+calsado = 42
+estudiando = True
+altur = 1.77
+
+#print ( "hola soy " ,nombre,  "tengo " , edad, "mi estado civil" , solter, "estudie en el ", colegio, " calso" , calsado, "aurita estoy", estudiando, "y mi altura es ", altur, )
+#edad = input("edad? ")
+#print((edad))
+
+
+#nombredeusuario = input("como es tu nombre?") 
+#print (nombredeusuario)
+
+if edad >= 18:
+    print("puedes entrar")
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
